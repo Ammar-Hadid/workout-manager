43f5a54ef3609cb80ac9e8f98b4252a4d6b67930
ElPridro <ammarhadid2005@gmail.com>
2026-05-02 16:44:17 +0200

    feat: create a program validation util

---
+++ backend/src/utils/validators/programValidator.js
@@ -0,0 +1,27 @@
+const validateProgram = ({ name, split, trainingDaysPerWeek }, isUpdate = false) => {
+    const errors = {};
+
+    if (!isUpdate || name !== undefined) {
+        if (!name || name.trim().length < 3) {
+            errors.name = 'Program names must be at least 3 characters long.';
+        }
+    }
+
+    if (!isUpdate || split !== undefined) {
+        if (!split) errors.split = 'Please select a split to structure your workouts.';
+    }
+
+    if (!isUpdate || trainingDaysPerWeek !== undefined) {
+        const days = Number(trainingDaysPerWeek);
+
+        if (isNaN(days)) errors.trainingDaysPerWeek = 'Please enter a number for training days per week.'
+
+        else if (!Number.isInteger(days)) errors.trainingDaysPerWeek = 'Please enter a whole number for training days per week.'
+
+        else if (days < 1 || days > 7) errors.trainingDaysPerWeek = `You entered [${trainingDaysPerWeek}]. Please pick a number of days between 1 and 7.`
+    }
+
+    return errors
+}
+
+export default validateProgram
