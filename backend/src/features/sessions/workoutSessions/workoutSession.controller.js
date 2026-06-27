aea1f09d0b280918cc53332ec25ed45d393b3e7e
ElPridro <ammarhadid2005@gmail.com>
2026-06-28 00:50:12 +0200

    feat: create getWorkoutSessionById controller

--- backend/src/features/sessions/workoutSessions/workoutSession.controller.js
+++ backend/src/features/sessions/workoutSessions/workoutSession.controller.js
@@ -5,7 +5,7 @@ import Workout from "../../workouts/Workout.model.js";
 import Exercise from "../../exercises/Exercise.model.js";
 
 import WorkoutSession from "./WorkoutSession.model.js";
-import ExerciseSession from "../../exercises/Exercise.model.js";
+import ExerciseSession from "../exerciseSessions/ExerciseSession.model.js";
 
 import { createExerciseSessionsFromExercises } from "../exerciseSessions/exerciseSession.service.js";
 
@@ -117,6 +117,40 @@ export const getActiveWorkoutSession = async (req, res) => {
         return res.status(200).json({
             workoutSession,
             exerciseSessions,
+        });
+    }
+
+    catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: 'Server error.' })
+    }
+}
+
+export const getWorkoutSessionById = async (req, res) => {
+    const { workoutSessionId } = req.params;
+
+    if (!mongoose.isValidObjectId(workoutSessionId)) {
+        return res.status(400).json({ error: 'Invalid workout session id.' })
+    }
+
+    try {
+        const workoutSession = await WorkoutSession.findOne({
+            user: req.userId,
+            _id: workoutSessionId,
+        });
+
+        if (!workoutSession) {
+            return res.status(404).json({ error: 'Workout session not found.' });
+        };
+
+        const exerciseSessions = await ExerciseSession.find({
+            user: req.userId,
+            workoutSession: workoutSession._id,
+        }).sort({ orderSnapshot: 1 });
+
+        return res.status(200).json({
+            workoutSession,
+            exerciseSessions
         });
     }
 
