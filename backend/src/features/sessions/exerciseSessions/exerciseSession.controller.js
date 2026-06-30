475f8cfa642454a791f6da3d503148a4fc1bda8b
ElPridro <ammarhadid2005@gmail.com>
2026-06-30 13:41:02 +0200

    feat: build complete exercise session controller

--- backend/src/features/sessions/exerciseSessions/exerciseSession.controller.js
+++ backend/src/features/sessions/exerciseSessions/exerciseSession.controller.js
@@ -44,3 +44,46 @@ export const startExerciseSession = async (req, res) => {
         return res.status(500).json({ error: 'Server error.' });
     }
 }
+
+export const completeExerciseSession = async (req, res) => {
+    const { workoutSessionId, exerciseSessionId } = req.params;
+
+    if (!mongoose.isValidObjectId(workoutSessionId)) {
+        return res.status(400).json({ error: 'Invalid workout session id.' });
+    }
+
+    if (!mongoose.isValidObjectId(exerciseSessionId)) {
+        return res.status(400).json({ error: 'Invalid exercise session id.' });
+    }
+
+    try {
+        const exerciseSession = await ExerciseSession.findOneAndUpdate(
+            {
+                user: req.userId,
+                workoutSession: workoutSessionId,
+                _id: exerciseSessionId,
+            },
+
+            {
+                status: 'completed',
+                completedAt: new Date(),
+            },
+
+            {
+                new: true,
+                runValidators: true,
+            }
+        );
+
+        if (!exerciseSession) {
+            return res.status(404).json({ error: 'Exercise session not found' });
+        }
+
+        return res.status(200).json({ exerciseSession });
+    }
+
+    catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: 'Server error.' })
+    }
+}
