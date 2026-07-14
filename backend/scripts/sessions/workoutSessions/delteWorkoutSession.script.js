26157dab380bf670631ea77ebebd996ab10f7056
ElPridro <ammarhadid2005@gmail.com>
2026-07-14 18:19:16 +0200

    feat: add CLI script to delete workout sessions safely

--- backend/scripts/sessions/workoutSessions/delteWorkoutSession.script.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-import WorkoutSession from "../../../src/features/sessions/workoutSessions/WorkoutSession.model.js"
-import WorkoutSession from "../../../src/features/sessions/exerciseSessions/ExerciseSession.model.js"
-
-const deleteWorkoutSession = ()
